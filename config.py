3c9a6aff07cc19ba9affac68fa302562eaf36d72
EshaniKatiyar <eshani1505@gmail.com>
2026-04-02 23:08:57 +0530

    Commit

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-import os
-
 DB_CONFIG = {
-    'host':     os.environ.get('MYSQLHOST', 'localhost'),
-    'port':     int(os.environ.get('MYSQLPORT', '3306')),
-    'user':     os.environ.get('MYSQLUSER', 'root'),
-    'password': os.environ.get('MYSQLPASSWORD', 'tiger'),
-    'database': os.environ.get('MYSQLDATABASE', 'Lost_And_Found_Management_System'),
+    'host':     'localhost',
+    'port':     3306,
+    'user':     'root',
+    'password': 'tiger',
+    'database': 'Lost_And_Found_Management_System',
     'charset':  'utf8mb4',
     'autocommit': False,
 }
