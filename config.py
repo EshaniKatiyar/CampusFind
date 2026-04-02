6c967c04fcee434765714ba7868c369121977045
EshaniKatiyar <eshani1505@gmail.com>
2026-04-02 22:49:30 +0530

    fix port

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 DB_CONFIG = {
     'host':     os.environ.get('MYSQLHOST', 'localhost'),
-    'port':     int(os.environ.get('MYSQLPORT', 3306)),
+    'port':     int(os.environ.get('MYSQLPORT', '3306')),
     'user':     os.environ.get('MYSQLUSER', 'root'),
     'password': os.environ.get('MYSQLPASSWORD', 'tiger'),
     'database': os.environ.get('MYSQLDATABASE', 'Lost_And_Found_Management_System'),
